51d4fe5b3fc03e85d676c74124043a10e38c8f2c
Luis Navarrete <luis.na06@gmail.com>
2015-12-20 22:45:12 -0500

    docs: Add despription to the Calories Health Tracker App

--- development/js/app.js
+++ development/js/app.js
@@ -18,7 +18,11 @@ function portfolio() {
             url: '//luisfdonavarrete.github.io/frontend-nanodegree-calories-health-tracker/',
             github: '//github.com/luisfdonavarrete/frontend-nanodegree-calories-health-tracker/',
             image: 'assets/img/health-tracker.png',
-            description: '' 
+            description: 'A single-page web application built using the Backbone and Firebase, ' +
+			'that tracks the user\'s calorie intake. Users can search for food items provided by ' +
+			'the Nutritionix API and select them. Once they are selected, they will be added to the ' +
+			'list of foods the user is tracking. The total calorie count will also update to reflect ' +
+			'the new daily total.' 
 		},
 		{
 			name: 'Neighborhood Map',
